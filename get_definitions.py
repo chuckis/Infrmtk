cebbbc7c393ba62185019a9f0ad9ad82c4e3e542
chuckis <chuckis94@gmail.com>
2021-10-28 00:16:57 +0300

    get_definitions

---
+++ get_definitions.py
@@ -0,0 +1,27 @@
+import os
+import re
+
+filepaths = []
+
+for root, dirs, files in os.walk("3-k", topdown = False):
+   for name in files:
+       filepaths.append(os.path.join(root, name))
+    #   print(os.path.join(root, name))
+#    for name in dirs:
+#       print(os.path.join(root, name))
+
+# print(filepaths)
+
+lines = []
+
+definitionregex = re.compile(r'\*{2}(.*?)\*{2}')
+definitions = ''
+
+# get all definitions from lessons
+for file in filepaths:
+    with open(file, 'r', encoding='utf8', errors='ignore') as lesson:
+        definition = definitionregex.findall(lesson.read())
+        if definition:
+            definitions+=str(definition).strip()
+            
+print(definitions)
