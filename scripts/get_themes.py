4b8839db7f70ff4e4703b0f2b6f3390b634871cc
chuckis <chuckis94@gmail.com>
2021-12-17 11:28:12 +0200

    3-k index

--- scripts/get_themes.py
+++ scripts/get_themes.py
@@ -1,22 +1,32 @@
 import os
+import re
 
 filepaths = []
 
-for subdir, dirs, files in os.walk("5-k"):
+for subdir, dirs, files in os.walk("3-k"):
     for filename in files:
         filepath = subdir + os.sep + filename
-        if filepath.endswith(".md"):
+        if filename.startswith("urok"):
             filepaths.append(filepath)
 
 lines = []
-filepaths.sort()
+
+def sort_nicely( l ):
+    """ Sort the given list in the way that humans expect.
+    """
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
+    l.sort( key=alphanum_key )
+
+sort_nicely(filepaths)
+
 for file in filepaths:
     with open(file, 'r', encoding='utf8', errors='ignore') as lesson:
         for line in lesson:
             if line.startswith("# "):
                 lines.append(line)
 
-filepaths.sort()
+
 print(filepaths)
 for line in lines:
     print(line)
