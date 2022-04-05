d714de10ea8c79c16ed9b9d1a1894451d8f27dec
chuckis <chuckis94@gmail.com>
2022-04-05 21:04:28 +0300

    moar, and variables for 7-k

---
+++ 7-k/src/variables.py
@@ -0,0 +1,60 @@
+# Variables are containers for storing data values.
+
+# x = 5
+# y = "Sergii"
+
+# print(x)
+# print(y)
+# print("Hello world!")
+
+# Для того щоб закоментувати або розкоментувати рядок коду, потрібно його виділити та натиснути поєднання клавіш "Ctr та /"
+
+# Variables do not need to be declared with any particular type, and can even change type after they have been set.
+
+x = 1
+x = "Sergii"
+
+# Завдання: вивести у консоль "вміст" змінної x
+
+# Ви можете вивести тип змінної з допомогою функції type()
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+# Імена змінних чутливі до регістру, тобто
+
+a = 4
+A = "Sally"
+
+# У цьому випадку A не зміниться
+
+# Існують правила для написання імен змінних
+# Це легальні способи
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# Camel Case - Верблюдів регістр
+# Each word, except the first, starts with a capital letter:
+# myVariableName = "John"
+
+# Pascal Case
+# Each word starts with a capital letter:
+# MyVariableName = "John"
+
+# Snake Case Зміїний регістр
+# Each word is separated by an underscore character:
+# my_variable_name = "John"
+
+# для тих кому цікаво, пошагові вправи за посиланням: https://www.w3schools.com/python/python_variables.asp
+
+# щоб розпочати з репліт https://www.youtube.com/watch?v=44Ckg47dR6g&ab_channel=KirZh
+
+# Щоб розпочати з Python, можна встановити додаток Pydroid3, 
+# https://play.google.com/store/apps/details?id=ru.iiec.pydroid3&hl=en&gl=US 
