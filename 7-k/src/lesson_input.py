e04562512edda9df7c44dddacebaf1e495f40610
chuckis <chuckis94@gmail.com>
2022-05-03 14:28:02 +0300

    lesson about input

---
+++ 7-k/src/lesson_input.py
@@ -0,0 +1,21 @@
+# Програма, що ілюструє роботу функцї input()
+
+if __name__=='__main__':
+    val = input("Введіть будь-яке значення: ")
+    print(val)
+
+    # Program to check input
+    # type in Python
+
+    num = input ("Введіть число: ")
+    print(num)
+    name1 = input("Введіть ім'я: ")
+    print(name1)
+
+    # Printing type of input value
+    print ("тип введеного числа ", type(num))
+    print ("тип введеного імені ", type(name1))
+
+
+def greeting():
+    pass
